e025b6ed74ecbc59243e8dae68206c45d61ad217
Geoffrey <geoffreysaldo@gmail.com>
2020-01-16 22:20:37 +0100

    Project set up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './App.css'
+
+import HomePage from './homepage.component'
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <HomePage/>
+    )
+  }
+}
+
+
+
+export default App
---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./homepage.styles.scss"
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">
+                    <h1>Actualité</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
